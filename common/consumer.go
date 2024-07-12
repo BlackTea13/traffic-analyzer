b246e54f09c8964726a67d7211d51f82949ccb8c
karank85 <kkjas85@gmail.com>
2024-07-12 16:54:07 +0700

    feat: consumer message from topic

--- common/consumer.go
+++ common/consumer.go
@@ -1,1 +1,42 @@
 package common
+
+import (
+	"context"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/twmb/franz-go/pkg/kgo"
+)
+
+type Consumer struct {
+	client *kgo.Client
+	topic  string
+}
+
+func NewConsumer(brokers []string, topic string) *Consumer {
+	groupID := uuid.New().String()
+	client, err := kgo.NewClient(
+		kgo.SeedBrokers(brokers...),
+		kgo.ConsumerGroup(groupID),
+		kgo.ConsumeTopics(topic),
+	)
+	if err != nil {
+		panic(err)
+	}
+	return &Consumer{client: client, topic: topic}
+}
+
+func (c *Consumer) ConsumeMessages() {
+	ctx := context.Background()
+	for {
+		fetches := c.client.PollFetches(ctx)
+		iter := fetches.RecordIter()
+		for !iter.Done() {
+			record := iter.Next()
+			fmt.Printf("%s\n", record.Value)
+		}
+	}
+}
+
+func (c *Consumer) Close() {
+	c.client.Close()
+}
