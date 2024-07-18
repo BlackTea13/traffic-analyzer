e90e3651c377fa729d181bd2689eb4e7abc650cb
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2024-07-18 16:39:46 +0700

    feat: set up influxdb

--- processor/internal/uploader.go
+++ processor/internal/uploader.go
@@ -2,18 +2,36 @@ package internal
 
 import (
 	"common"
+	"context"
+	// "encoding/json"
 	"fmt"
+	// "log"
+	"time"
 
 	"github.com/influxdata/influxdb-client-go/v2"
+	"github.com/influxdata/influxdb-client-go/v2/api"
 	"github.com/twmb/franz-go/pkg/kgo"
 )
 
 type Uploader struct {
 	Consumer *common.Consumer
 	Client   influxdb2.Client
+	WriteAPI api.WriteAPIBlocking
 }
 
 func (u *Uploader) Upload(record *kgo.Record) {
+	// var packet common.EnrichedPacket
+	// err := json.Unmarshal(record.Value, &packet)
+	// if err != nil {
+	// 	log.Printf("Error unmarshalling JSON: %v\n", err)
+	// 	return
+	// }
+	p := influxdb2.NewPoint("stat",
+		map[string]string{"unit": "temperature"},
+		map[string]interface{}{"avg": 24.5, "max": 45},
+		time.Now())
+	u.WriteAPI.WritePoint(context.Background(), p)
+	u.WriteAPI.Flush(context.Background())
 }
 
 func (u *Uploader) Run() {
@@ -26,16 +44,19 @@ func (u *Uploader) Close() {
 }
 
 func NewUploader(brokers []string, topic string, dbAddress string) *Uploader {
+	client := influxdb2.NewClient(dbAddress, "replace-me-later")
 	return &Uploader{
 		Consumer: common.NewConsumer(brokers, topic),
-		Client:   influxdb2.NewClient(dbAddress, "replace-me-later"),
+		Client:   client,
+		WriteAPI: client.WriteAPIBlocking("org", "bucket"),
 	}
 }
 
 func InfluxPenetrator(kafkaBrokers string, consumeTopic string, dbAddress string) {
 	uploader := NewUploader([]string{kafkaBrokers}, consumeTopic, dbAddress)
 	fmt.Println("Influx Penetrator starting up...")
-	uploader.Run()
-	defer uploader.Close()
+	uploader.Upload(nil)
+	// uploader.Run()
+	// defer uploader.Close()
 
 }
